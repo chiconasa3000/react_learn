927c226ceb76c336a98256354fafe4c94e794d9a
chiconasa3000 <csucavelando@gmail.com>
2024-01-04 15:23:24 -0500

    jsx changes

--- projects/app_store/src/Gallery.jsx
+++ projects/app_store/src/Gallery.jsx
@@ -1,9 +1,51 @@
 export function Profile(){
+  
+  // today
+  const today = new Date();
+  let options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    timeZone: "America/Lima",
+    timeZoneName: "short",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+  };
+
+  //js object
+  const person = {
+    avatar: "https://i.imgur.com/yXOvdOSs.jpg",
+    description: "Hedy Lamarr",
+    name: "Hedy Lamarr",
+    theme: {
+      backgroundColor: 'black',
+      color: 'green',
+    }
+  }
+ 
+  function formatDate(date){
+    return new Intl.DateTimeFormat(
+      'en-US',
+      options,
+    ).format(date);
+  } 
+
   return(
-    <img
-      src="https://i.imgur.com/MK3eW3As.jpg"
-      alt="Katherine Jhonson"
-    />
+    <>
+      <h1>{person.name}'s Todos for {formatDate(today)}</h1>
+      <img
+        src={person.avatar}
+        alt={person.description}
+        className = "photo"
+      />
+      <ul style={person.theme}>
+        <li>Invent new traffic lights </li>
+        <li>Rehearse a movie scene</li>
+        <li>Improve the spectrum technology</li>
+      </ul>
+    </>
   );
 }
 
